0ea6a7445cb73ec87745c5e8e3ea89b0043a3835
marwajomaa <marwajomaa4@gmail.com>
2021-03-07 14:50:23 +0200

    change signup function

--- src/pages/Auth/signup.js
+++ src/pages/Auth/signup.js
@@ -11,89 +11,110 @@ function Signup() {
   const history = useHistory();
   const [values, setValues] = useState({});
   const [error, setError] = useState("");
+  const [msg, setMsg] = useState("");
+  const [createdUser, setCreateUser] = useState();
+  const [continueSignup, setContinueSignup] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
   };
+
   const handleSignUp = async (e) => {
     e.preventDefault();
-    const usernameExists = await doesUsernameExist(values.username);
-    if (!usernameExists.length) {
+    try {
       const createdUser = await auth.createUserWithEmailAndPassword(
         values.email,
         values.password
       );
 
-      await createdUser.user.updateProfile({
-        displayName: values.username,
-      });
-
-      const newUser = {
-        userId: createdUser.user.uid,
-        username: values.username.toLowerCase(),
-        emailAddress: values.email.toLowerCase(),
-        phoneNumber: values.phoneNum,
-        password: values.password,
-        token: createdUser.user.refreshToken,
-      };
-
-      await firestore.collection("users").add(newUser);
-      try {
-        const newLocal = `${process.env.REACT_APP_LOCAL_URL}/api/users/save-user`;
-        const res = await axios.post(newLocal, newUser);
-        console.log(res);
-      } catch (err) {
-        setError(err.message);
+      if (createdUser.user.refreshToken) {
+        setContinueSignup(true);
+        setCreateUser(createdUser);
       }
-      history.push(ROUTES.LANDING);
-    } else {
       setValues({ ...values, username: "" });
-      setError("That username is already taken, please try another.");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const saveUser = async (e) => {
+    console.log(createdUser, "cccccccccccccccccccccccccccccc");
+    e.preventDefault();
+    const newUser = {
+      userId: createdUser.user.uid,
+      username: values.username.toLowerCase(),
+      emailAddress: values.email.toLowerCase(),
+      phoneNumber: values.phoneNum,
+      password: values.password,
+      token: createdUser.user.refreshToken,
+    };
+
+    console.log(newUser, "uuuuuuuuuuuuuuuuuuuuuuuuuuuser");
+
+    try {
+      const newLocal = "http://localhost/8000/api/users/save-user";
+      const res = await axios.post(newLocal, newUser);
+      console.log(res);
+      setMsg(res.data.msg);
+      history.push(ROUTES.LANDING);
+    } catch (err) {
+      console.log(err);
+      setError(err.message);
     }
   };
 
   return (
     <div>
+      <h3>Sign up</h3>
       <div>
-        {error && <p>{error}</p>}
-
-        <form onSubmit={handleSignUp}>
-          <Input
-            aria-label="Enter your username"
-            name="username"
-            type="text"
-            placeholder="Username"
-            onChange={(e) => handleInputChange(e)}
-            value={values.username}
-          />
-          <Input
-            aria-label="Enter your email address"
-            name="email"
-            type="email"
-            placeholder="Email address"
-            onChange={(e) => handleInputChange(e)}
-            value={values.email}
-          />
-          <Input
-            aria-label="Enter your phone number"
-            name="phoneNum"
-            type="text"
-            placeholder="Phone number"
-            onChange={(e) => handleInputChange(e)}
-            value={values.phoneNum}
-          />
-          <Input
-            aria-label="Enter your password"
-            name="password"
-            type="password"
-            placeholder="Password"
-            onChange={(e) => handleInputChange(e)}
-            value={values.password}
-          />
-          <button type="submit">Sign Up</button>
-        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {msg && <p>{msg}</p>}
+        {!continueSignup && (
+          <form onSubmit={handleSignUp}>
+            <Input
+              aria-label="Enter your email address"
+              name="email"
+              type="email"
+              placeholder="Email address"
+              onChange={(e) => handleInputChange(e)}
+              value={values.email}
+            />
+            <Input
+              aria-label="Enter your password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => handleInputChange(e)}
+              value={values.password}
+            />
+            <button type="submit">continue</button>
+          </form>
+        )}
       </div>
+      {continueSignup && (
+        <form onSubmit={saveUser}>
+          <>
+            <Input
+              aria-label="Enter your username"
+              name="username"
+              type="text"
+              placeholder="Username"
+              onChange={(e) => handleInputChange(e)}
+              value={values.username}
+            />
+            <Input
+              aria-label="Enter your phone number"
+              name="phoneNum"
+              type="text"
+              placeholder="Phone number"
+              onChange={(e) => handleInputChange(e)}
+              value={values.phoneNum}
+            />
+            <button>Signup</button>
+          </>
+        </form>
+      )}
     </div>
   );
 }
